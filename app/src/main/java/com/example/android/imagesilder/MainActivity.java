0c24e1ae08f78da882bdfb839f3db6d452623ee5
pual035 <ankurpal1997@gmail.com>
2020-05-09 13:43:30 +0530

    add indicators

--- app/src/main/java/com/example/android/imagesilder/MainActivity.java
+++ app/src/main/java/com/example/android/imagesilder/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.android.imagesilder;
 
 import android.os.Handler;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import java.util.Timer;
@@ -31,9 +34,30 @@ public class MainActivity extends AppCompatActivity {
         viewPager = (ViewPager)findViewById(R.id.view_pager);
         customSwipeAdapter = new CustomSwipeAdapter(this, image_resource);
         viewPager.setAdapter(customSwipeAdapter);
-
+        //Prepare dots layout
+        prepareDots(custom_position++);
         //After the view pager is set we call the createSlideShow method for automatic swiping
         createSlideShow();
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if(custom_position > 4){
+                    custom_position = 0;
+                }
+                prepareDots(custom_position++);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 
     //This method will automatically slide the images
@@ -56,5 +80,25 @@ public class MainActivity extends AppCompatActivity {
                 handler.post(runnable);
             }
         },250, 1500);
+    }
+
+    private void prepareDots(int currentSlidePosition){
+        //If image view is already present then remove all views
+        if(dotsLayout.getChildCount() > 0){
+            dotsLayout.removeAllViews();
+        }
+        ImageView dots[] = new ImageView[5];
+        for(int i = 0; i < 5; i++){
+            dots[i] = new ImageView(this);
+            if(i == currentSlidePosition){
+                dots[i].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.active_dots));
+            }else{
+                dots[i].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.inactive_dots));
+            }
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams
+                    (ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            layoutParams.setMargins(4, 0, 4, 0);
+            dotsLayout.addView(dots[i], layoutParams);
+        }
     }
 }
